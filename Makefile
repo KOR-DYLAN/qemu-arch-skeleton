41a78a4f1363d0cebfc5d56090c041980eafca57
JunKi(Dylan), Hong <dylanhong920509@gmail.com>
2025-09-13 15:57:39 +0900

    implement basic build system

--- Makefile
+++ Makefile
@@ -1,11 +1,21 @@
 TOPDIR			:=$(dir $(abspath $(firstword $(MAKEFILE_LIST))))
 WORKSPACE		:=$(TOPDIR)
-OUTPUT			:=$(TOPDIR)/build
-# TOOLCHAIN		:=aarch64-none-linux-gnu
-TOOLCHAIN		:=aarch64-none-linux-llvm
+BUILD			:=$(TOPDIR)/build
 CMAKE			:=cmake
 NPROC			?=$(shell nproc)
 V				?=0
+
+# qemu-aarch64
+QEMU			:=qemu-aarch64
+
+# aarch64-none-linux-llvm | aarch64-none-linux-gnu
+TOOLCHAIN		:=aarch64-none-linux-llvm
+
+# Debug | Release | RelWithDebInfo | MinSizeRel
+BUILD_TYPE		:=Debug
+
+# Target Name
+TARGET			:=sample
 
 ifneq ($(TOOLCHAIN),)
     CMAKE_FLAGS	+=-DCMAKE_TOOLCHAIN_FILE=$(TOPDIR)/script/toolchain/$(TOOLCHAIN).cmake
@@ -17,18 +27,23 @@ endif
 
 phony+=config
 config:
-	$(CMAKE) $(CMAKE_FLAGS) -S$(WORKSPACE) -B$(OUTPUT)
+	$(CMAKE) $(CMAKE_FLAGS) -DCMAKE_BUILD_TYPE=$(BUILD_TYPE) -S$(WORKSPACE) -B$(BUILD)
 
 phony+=build
 build:
-	$(CMAKE) --build $(OUTPUT) -j $(NPROC) $(VERBOSE)
+	$(CMAKE) --build $(BUILD) -j $(NPROC) $(VERBOSE)
 
 phony+=clean
 clean:
-	$(CMAKE) --build $(OUTPUT) --target clean $(VERBOSE)
+	$(CMAKE) --build $(BUILD) --target clean $(VERBOSE)
 
 phony+=distclean
 distclean:
-	rm -rf $(OUTPUT)
+	rm -rf $(BUILD)
+
+phony+=run
+run: build
+	$(eval SYSROOT := $(shell cat $(BUILD)/sysroot.txt))
+	$(QEMU) -L $(SYSROOT) $(BUILD)/$(BUILD_TYPE)/bin/$(TARGET)
 
 .PHONY: $(phony)
--- script/toolchain/llvm.cmake
+++ script/toolchain/llvm.cmake
@@ -1,3 +1,5 @@
+include(${CMAKE_CURRENT_LIST_DIR}/../misra/llvm.cmake)
+
 # Set Toolchin
 set(CMAKE_ASM_COMPILER  clang)
 set(CMAKE_C_COMPILER    clang)
@@ -21,6 +23,7 @@ execute_process(
 )
 if(GCC_SYSROOT)
     set(CMAKE_SYSROOT "${GCC_SYSROOT}" CACHE PATH "Sysroot path" FORCE)
+    file(WRITE ${CMAKE_BINARY_DIR}/sysroot.txt "${GCC_SYSROOT}\n")
     message(STATUS "Sysroot detected: ${CMAKE_SYSROOT}")
 else()
     message(WARNING "Failed to detect sysroot from ${CMAKE_C_COMPILER}")
@@ -35,13 +38,32 @@ execute_process(
 get_filename_component(GCC_TOOLCHAIN_BIN "${GCC_TOOLCHAIN}" DIRECTORY)
 get_filename_component(GCC_TOOLCHAIN_ROOT "${GCC_TOOLCHAIN_BIN}" DIRECTORY)
 
-set(LLVM_TARGET_AND_SYSROOT "--target=${TARGET_TRIPLE} --sysroot=${CMAKE_SYSROOT} --gcc-toolchain=${GCC_TOOLCHAIN_ROOT}")
-set(CMAKE_C_FLAGS_INIT             "${LLVM_TARGET_AND_SYSROOT}")
-set(CMAKE_CXX_FLAGS_INIT           "${LLVM_TARGET_AND_SYSROOT}")
-set(CMAKE_ASM_FLAGS_INIT           "${LLVM_TARGET_AND_SYSROOT}")
-set(CMAKE_EXE_LINKER_FLAGS_INIT    "${LLVM_TARGET_AND_SYSROOT} -fuse-ld=bfd")
-set(CMAKE_SHARED_LINKER_FLAGS_INIT "${LLVM_TARGET_AND_SYSROOT} -fuse-ld=bfd")
-set(CMAKE_MODULE_LINKER_FLAGS_INIT "${LLVM_TARGET_AND_SYSROOT} -fuse-ld=bfd")
+set(CMAKE_C_FLAGS_INIT              "--gcc-toolchain=${GCC_TOOLCHAIN_ROOT}")
+set(CMAKE_CXX_FLAGS_INIT            "--gcc-toolchain=${GCC_TOOLCHAIN_ROOT}")
+set(CMAKE_ASM_FLAGS_INIT            "--gcc-toolchain=${GCC_TOOLCHAIN_ROOT}")
+set(CMAKE_EXE_LINKER_FLAGS_INIT     "--gcc-toolchain=${GCC_TOOLCHAIN_ROOT} -fuse-ld=bfd")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT  "--gcc-toolchain=${GCC_TOOLCHAIN_ROOT} -fuse-ld=bfd")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT  "--gcc-toolchain=${GCC_TOOLCHAIN_ROOT} -fuse-ld=bfd")
+
+# Debug
+set(CMAKE_ASM_FLAGS_DEBUG           "-O0 -g")
+set(CMAKE_C_FLAGS_DEBUG             "-O0 -g ${MISRA_C_WARN}")
+set(CMAKE_CXX_FLAGS_DEBUG           "-O0 -g ${MISRA_CXX_WARN}")
+
+# MinSizeRel
+set(CMAKE_ASM_FLAGS_MINSIZEREL      "-Os -DNDEBUG")
+set(CMAKE_C_FLAGS_MINSIZEREL        "-Os -DNDEBUG ${MISRA_C_WARN}")
+set(CMAKE_CXX_FLAGS_MINSIZEREL      "-Os -DNDEBUG ${MISRA_CXX_WARN}")
+
+# Release
+set(CMAKE_ASM_FLAGS_RELEASE         "-O2 -DNDEBUG")
+set(CMAKE_C_FLAGS_RELEASE           "-O2 -DNDEBUG ${MISRA_C_WARN}")
+set(CMAKE_CXX_FLAGS_RELEASE         "-O2 -DNDEBUG ${MISRA_CXX_WARN}")
+
+# RelWithDebInfo
+set(CMAKE_ASM_FLAGS_RELWITHDEBINFO  "-O2 -g -DNDEBUG")
+set(CMAKE_C_FLAGS_RELWITHDEBINFO    "-O2 -g -DNDEBUG ${MISRA_C_WARN}")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "-O2 -g -DNDEBUG ${MISRA_CXX_WARN}")
 
 set(CMAKE_FIND_ROOT_PATH "${CMAKE_SYSROOT}")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
